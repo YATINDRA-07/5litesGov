79adb794a133d41a850aa8da9c9e037f797ad9ec
radha <shraogiradha@gmai.com>
2020-04-04 16:33:23 +0530

    goverment ui

--- app/src/main/java/com/yatindra/a5litesgov/MainActivity.java
+++ app/src/main/java/com/yatindra/a5litesgov/MainActivity.java
@@ -3,12 +3,23 @@ package com.yatindra.a5litesgov;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
     }
+
+
+
 }
