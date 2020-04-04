231b62107e7977e7ac26d7716b75afead8ae4acd
CyberWick <adikale123@gmail.com>
2020-04-04 23:34:42 +0530

    Added DynamoDB dependencies

--- app/src/main/java/com/yatindra/a5litesgov/MainActivity.java
+++ app/src/main/java/com/yatindra/a5litesgov/MainActivity.java
@@ -15,9 +15,31 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 // Do something in response to button click
             }
         });
+        //Changes:
+
+//        getDatabase();
+//        GetAllItemsAsyncTask getAllItemsAsyncTask = new GetAllItemsAsyncTask();
+//        try{
+////            Document i = getAllItemsAsyncTask.execute("d4:63:c6:77:92:52").get();
+//            Document document = new Document();
+//            document.put("MACid", "d4:63:c6:77:92:52");
+//            document.put("status", "negative");
+//            document.put("phone", "12345678");
+//            Document ping = new Document(), allPings = new Document();
+//            ping.put("counter", 1);
+//            ping.put("timestamp", "04-04-2020-20:05");
+//            allPings.put("newmac", ping);
+//            document.put("Pings", all);
+////            System.out.println("Size: " + i.size());
+//            Integer temp = new PutDataAsyncTask().execute(document).get();
+//            System.out.println("O/P: " + temp);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
     }
 
 
---
+++ app/src/main/java/com/yatindra/a5litesgov/model/RegisterNode.java
@@ -0,0 +1,18 @@
+package com.yatindra.a5litesgov.model;
+
+import android.provider.DocumentsContract;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document;
+
+public class RegisterNode {
+
+    public static Document registerUSer(String phone, String status, String MACid){
+        Document doc = new Document();
+        doc.put("MACid", MACid);
+        doc.put("phone", phone);
+        doc.put("status", status);
+        return doc;
+    }
+
+
+}
